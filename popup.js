0800fbcf8ff6fd30ac729e303ec207bd25bc027c
DomThePorcupine <porcupinebot@gmail.com>
2015-08-16 14:59:48 -0400

    Switched to JSON

--- popup.js
+++ popup.js
@@ -1,66 +1,16 @@
-var img = new Image();
-var theHTML = "";
-var tempHTML ="";
-var div = document.getElementById('body');
-var anotherString = "";
-var thirdString = "";
-
+var theImg = new Image();
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "https://xkcd.com/", true);
+var url = "http://xkcd.com/info.0.json";
+xmlhttp.open("GET", url, false);
+var theJSON;
+xmlhttp.addEventListener("load", function(e) {
+	theJSON = JSON.parse(xmlhttp.responseText);
+}, false)
+
+
 xmlhttp.send(null);
-var truth = true;
-setTimeout(function(){
-	theHTML = xmlhttp.responseText;
-	var lines = theHTML.split('\n');
-	for (var i = lines.length - 1; i >= 0; i--) {
-		if(lines[i].indexOf("title=") > -1 && truth)
-		{
-			tempHTML = lines[i];
-			var moreLines = tempHTML.split("\"");
-			truth = false;
-			for (var j = 0; j <= moreLines.length - 1; j++) {
-				if(moreLines[j].indexOf("src") > -1)
-				{
-					thirdString = moreLines[j+1];
-					img.src = thirdString;
-					img_home.appendChild(img);
-				}
-				if(moreLines[j].indexOf("title") > -1)
-				{
-					anotherString = moreLines[j+1];
-					if(anotherString.indexOf("&quot") > -1)
-					{
-						anotherString = anotherString.replace("&quot;", "\"");
-						var dammit = anotherString.split("&quot;");
-						anotherString = "";
-						for(var k = 0; k < dammit.length-1; k++)
-						{
-							dammit[k] = dammit[k] + "\"";
-							anotherString = anotherString + dammit[k]
-						}
-					}
-					if(anotherString.indexOf("&#39;") > -1)
-					{
-						anotherString = anotherString.replace("&#39;", "\'");
-						var dammit = anotherString.split("&#39;");
-						anotherString = "";
-						for(var k = 0; k < dammit.length-1; k++)
-						{
-							dammit[k] = dammit[k] + "\'";
-							anotherString = anotherString + dammit[k]
-						}
-					}
-					var x = document.createElement("P");
-					var t = document.createTextNode(anotherString);
-					x.appendChild(t);                           
-					document.body.appendChild(x);
-
-					var dic = document.getElementById('download');
-					dic.innerHTML =  dic.innerHTML + "<a href=\"" + thirdString + "\" download=\"\" title=\"Download today's XKCD\"><img src=\"download.png\" alt=\"ImageName\"></a>";
-				}
-				
-			};
-
-		}
-	};
-}, 750);
+theTitle.innerHTML = theJSON.safe_title;
+theImg.src = theJSON.img;
+img_home.appendChild(theImg);
+text.innerHTML = theJSON.alt;
+download.innerHTML = "<a href=\"" + theJSON.img + "\" download=\"\" title=\"Download today's XKCD\"><img src=\"download.png\" alt=\"ImageName\"></a>";
